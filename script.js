2340482385dd39e64d33df157694c84093438528
xenionx <hank.chou16@gmail.com>
2023-08-21 04:50:20 +0800

    rps ui version 1.0

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-
+const buttons = document.querySelectorAll('button');
+const score = document.querySelector('.score');
+const msg = document.querySelector('.message');
+const restart = document.querySelector('.restart')
 const choices = ['Rock', 'Paper', 'Scissors'];
 let computerScore = 0;
 let playerScore = 0;
 
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        let playerSelection = button.className;
+        let computerSelection = getComputerChoice();
+        playRound(playerSelection, computerSelection);
+    })
+});
 
 function getComputerChoice(){
     let random = Math.floor(Math.random() * choices.length);
@@ -11,29 +22,54 @@ function getComputerChoice(){
 
 function playRound(playerSelection, computerSelection){
     const capitalizeSelection  = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
-    if(capitalizeSelection == computerSelection){
-        computerScore += 0.5;
-        playerScore += 0.5;
-        return("It's a Draw!");
-    }else if (playerSelection == "Rock" && computerSelection == "Scissors" || playerSelection == "Paper" && computerSelection == "Rock"||playerSelection == "Scissors" && computerSelection == "Paper" ){
-        playerScore += 1;
-        return("Congrats, you win!");
-    }else if (playerSelection == "Rock" && computerSelection == "Paper" || playerSelection == "Paper" && computerSelection == "Scissors" || playerSelection == "Scissors" && computerSelection == "Rock"){
-        computerScore += 1;
-        return("You lose!");
-    }else{
-        return("You input the wrong value, I'll give you 0");
+    if(playerScore < 5 && computerScore < 5){
+        if(capitalizeSelection == computerSelection){
+            computerScore += 0.5;
+            playerScore += 0.5;
+            score.textContent = `It's a draw, the score is now ${playerScore}(You) vs '${computerScore}'(Computer)`;
+        }else if (playerSelection == "Rock" && computerSelection == "Scissors" || playerSelection == "Paper" && computerSelection == "Rock"||playerSelection == "Scissors" && computerSelection == "Paper" ){
+            playerScore += 1;
+            score.textContent = `You win this round! ${playerScore}(You) vs '${computerScore}'(Computer)`;
+        }else if (playerSelection == "Rock" && computerSelection == "Paper" || playerSelection == "Paper" && computerSelection == "Scissors" || playerSelection == "Scissors" && computerSelection == "Rock"){
+            computerScore += 1;
+            score.textContent = `You lose this round, the score is now ${playerScore}(You) vs '${computerScore}'(Computer)`;
+        }
+    }
+    else{
+        if(playerScore > computerScore){
+            msg.textContent = "You have won this game!";
+            msg.style.color = '#03fc6b';
+            console.log(msg.textContent);
+            score.textContent = `Final score is ${playerScore} (You) vs ${computerScore} (Computer)`
+        }
+        else if(playerScore == 5 && computerScore == 5){
+            msg.textContent = "It's a draw!";
+            console.log(msg.textContent);
+            msg.style.color = 'skyblue';
+            score.textContent = `Final score is ${playerScore} (You) vs ${computerScore} (Computer)`
+        }
+        else{   
+            msg.textContent = "You have lost this game!";
+            console.log(msg.textContent);
+            msg.style.color = 'red';
+            score.textContent = `Final score is ${playerScore} (You) vs ${computerScore} (Computer)`
+        }
+        resetGame();
     }
 }
 
-function game(){
-    for(let i = 0; i < 5; i++){
-        const playerSelection = prompt("What's your choice?");
-        const computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
-    }
-    console.log("The final score is: Player - " + playerScore + " | " + "Computer - " + computerScore);
+function resetGame(){
+    buttons.forEach((button) =>{
+        button.disabled = true;
+    })
+    restart.disabled = false;
+    computerScore = 0;
+    playerScore = 0;
 }
 
-
-game();
+restart.addEventListener('click', () => buttons.forEach((button) =>{
+    button.disabled = false;
+    restart.disabled = true;
+    msg.textContent = "";
+    score.textContent = "";
+}))
